ea3037f750ad3dfc372c7f6c9d3e6195098a7d62
Kristof Willen <kristof.willen@engie.com>
2022-04-28 15:14:01 +0200

    normalize setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,30 @@ setup(
     url="https://github.com/kristofwillen/cfntagger",
     license="GPL",
     keywords="cloudformation tagging",
-    install_requires=['ruamel.yaml', 'colorama', 'gitpython'],
-    tests_require=["pytest-cov", "cfn-lint", 'pytest'],
+    install_requires=[
+        'ruamel.yaml==0.17.21', 
+        'colorama>=0.4.4', 
+        'gitpython>=3.1.27'
+    ],
+    tests_require=[
+        'pytest-cov>=3.0.0', 
+        'cfn-lint>=0.58.1', 
+        'pytest>=7.1.1'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
 )
