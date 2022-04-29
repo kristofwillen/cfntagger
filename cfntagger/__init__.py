06e3f6e11f18d9a821290b688434dcd97276ffea
Kristof Willen <kristof.willen@engie.com>
2022-04-29 16:19:17 +0200

    Fix some packaging issues

---
+++ cfntagger/__init__.py
@@ -0,0 +1,1 @@
+from .cfntagger import Tagger
--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup, find_packages
+
 setup(
     name="cfntagger",
-    version="0.9.0",
+    version="0.9.2",
     packages=find_packages(),
-    scripts=['cfntagger'],
+    scripts=['cfntagger/cfntagger'],
 
     # metadata for upload to PyPI
     author="Kristof Willen",
@@ -13,29 +14,13 @@ setup(
     license="GPL",
     keywords="cloudformation tagging",
     install_requires=[
-        'ruamel.yaml==0.17.21', 
-        'colorama>=0.4.4', 
+        'ruamel.yaml==0.17.21',
+        'colorama>=0.4.4',
         'gitpython>=3.1.27'
     ],
     tests_require=[
-        'pytest-cov>=3.0.0', 
-        'cfn-lint>=0.58.1', 
+        'pytest-cov>=3.0.0',
+        'cfn-lint>=0.58.1',
         'pytest>=7.1.1'
-    ],
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3 :: Only',
     ],
 )
