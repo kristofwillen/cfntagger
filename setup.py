f440f6554610bb71e2a3cdaa9a8027b60fd55a46
Kristof Willen <kristof.willen@engie.com>
2022-07-15 06:38:58 +0200

    prep v0.9.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cfntagger",
-    version="0.9.6",
+    version="0.9.7",
     packages=find_packages(),
     scripts=['cfntagger/cfntagger'],
 
