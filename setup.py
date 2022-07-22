e909d4e1c0d9aaa960d0e7825050c2c0d1909a1c
Kristof Willen <kristof@rocinante.local>
2022-07-22 12:02:55 +0200

    Prep 0.9.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cfntagger",
-    version="0.9.7",
+    version="0.9.8",
     packages=find_packages(),
     scripts=['cfntagger/cfntagger'],
 
