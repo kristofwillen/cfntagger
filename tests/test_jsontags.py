853748a4102c2d7c7879a9652aa54d20911cebae
Kristof Willen <kristof@rocinante.local>
2023-07-25 22:38:21 +0200

    Add more tests on json tags

--- tests/test_jsontags.py
+++ tests/test_jsontags.py
@@ -6,7 +6,7 @@ from tests.unittest import CfnTestTemplate
 
 @pytest.fixture
 def mock_env_single_custom_tag(monkeypatch):
-    monkeypatch.setenv("CFN_TAGS", '{"Creator": "kristof"}')
+    monkeypatch.setenv("CFN_TAGS", '{"Environment": "Development"}')
 
 cfn_template = "./tests/templates/jsontags.yml"
 testcase = CfnTestTemplate(cfn_template)
@@ -17,6 +17,23 @@ def test_json_tags(mock_env_single_custom_tag):
     cfn_tagger.tag()
 
     assert len(cfn_tagger.get_found_tags("ResourceWithTags")) == 1
+    assert len(cfn_tagger.get_added_tags("ResourceWithTags")) == 1
+    assert len(cfn_tagger.get_updated_tags("ResourceWithTags")) == 0
+    assert cfn_tagger.get_added_tags("ResourceWithTags") == ['Environment']
+
+    assert len(cfn_tagger.get_found_tags("ResourceWithoutTags")) == 0
+    assert len(cfn_tagger.get_added_tags("ResourceWithoutTags")) == 1
+    assert len(cfn_tagger.get_updated_tags("ResourceWithoutTags")) == 0
+    assert cfn_tagger.get_added_tags("ResourceWithoutTags") == ['Environment']
+
+    assert len(cfn_tagger.get_found_tags("ResourceWithoutJsonTags")) == 0
+    assert len(cfn_tagger.get_added_tags("ResourceWithoutJsonTags")) == 1
+    assert len(cfn_tagger.get_updated_tags("ResourceWithoutJsonTags")) == 0
+    assert cfn_tagger.get_added_tags("ResourceWithoutJsonTags") == ['Environment']
+
     assert len(cfn_tagger.get_found_tags("ResourceWithJsonTags")) == 4
-    assert cfn_tagger.get_added_tags("ResourceWithTags") == ['Creator']
-    assert cfn_tagger.get_added_tags("ResourceWithJsonTags") == ['Creator']
+    assert len(cfn_tagger.get_added_tags("ResourceWithJsonTags")) == 0
+    assert len(cfn_tagger.get_updated_tags("ResourceWithJsonTags")) == 1
+    assert cfn_tagger.get_updated_tags("ResourceWithJsonTags") == ['Environment']
+
+
