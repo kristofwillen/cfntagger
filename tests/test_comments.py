945552eff65f57ef0a361ac8334880f01563099e
Kristof Willen <kristof.willen@gmail.com>
2024-06-27 23:38:32 +0200

    Updated deps

--- tests/test_comments.py
+++ tests/test_comments.py
@@ -14,7 +14,7 @@ def mock_env_single_custom_tag(monkeypatch):
 cfn_template = "./tests/templates/comments.yml"
 testcase = CfnTestTemplate(cfn_template)
 
-@pytest.mark.skip(reason="waiting for upstream to fix this")
+#@pytest.mark.skip(reason="waiting for upstream to fix this")
 def test_comments(mock_env_single_custom_tag):
     cfn_tagger = Tagger(filename=cfn_template, simulate=True)
 
